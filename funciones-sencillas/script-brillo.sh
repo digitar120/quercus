85cce4af9e95488b49bc7f828788dfd7e3f5f79f
digitar120 <digitar120@outlook.com>
2023-08-07 10:01:33 -0300

    script-brillo.sh ahora puede devolver brillo actual o máximo en números crudos

--- funciones-sencillas/script-brillo.sh
+++ funciones-sencillas/script-brillo.sh
@@ -4,9 +4,11 @@
 ARCHIVO=$DIRECTORIO_BRILLO/brightness
 
 if [ $1 == "get" ]; then
-	cat $ARCHIVO
-elif [ $1 == "get_max" ]; then
-	cat $DIRECTORIO_BRILLO/max_brightness
+	if [ $2 == "current" ]; then
+		cat $ARCHIVO
+	elif [ $2 == "max" ]; then
+		cat $DIRECTORIO_BRILLO/max_brightness
+	fi
 elif [ $1 == "set" ]; then
 	echo $2 > $ARCHIVO
 elif [ $1 == "add" ]; then
