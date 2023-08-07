b67220a0bd8b4f98594d80b7113a9acd1571c8b8
digitar120 <digitar120@outlook.com>
2023-08-07 10:04:15 -0300

    Cambios menores a scrip-brillo.sh

--- funciones-sencillas/script-brillo.sh
+++ funciones-sencillas/script-brillo.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 . ../configuraciones/variables.sh
-ARCHIVO=$DIRECTORIO_BRILLO/brightness
+BRILLO_ACTUAL=$DIRECTORIO_BRILLO/brightness
+BRILLO_MAXIMO=$DIRECTORIO_BRILLO/max_brightness
 
 if [ $1 == "get" ]; then
 	if [ $2 == "current" ]; then
-		cat $ARCHIVO
+		cat $BRILLO_ACTUAL
 	elif [ $2 == "max" ]; then
-		cat $DIRECTORIO_BRILLO/max_brightness
+		cat $BRILLO_MAXIMO
 	fi
 elif [ $1 == "set" ]; then
-	echo $2 > $ARCHIVO
+	echo $2 > $BRILLO_ACTUAL
 elif [ $1 == "add" ]; then
-	awk -v val=$2 '{print $1 + val}' $ARCHIVO > $ARCHIVO
+	awk -v val=$2 '{print $1 + val}' $BRILLO_ACTUAL > $BRILLO_ACTUAL
 fi
