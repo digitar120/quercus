6ca44a48cb3b6da5d0ca2e42a97d9cf9cc8b64ef
digitar120 <digitar120@outlook.com>
2023-08-12 03:39:15 -0300

    Cambios a script-brillo para uso de números de 1 al 100

--- funciones-sencillas/script-brillo.sh
+++ funciones-sencillas/script-brillo.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 . ../configuraciones/variables.sh
-BRILLO_ACTUAL=$DIRECTORIO_BRILLO/brightness
-BRILLO_MAXIMO=$DIRECTORIO_BRILLO/max_brightness
-
+BRA_DIR=$DIRECTORIO_BRILLO/brightness
+BRILLO_ACTUAL=$(cat $DIRECTORIO_BRILLO/brightness)
+BRILLO_MAXIMO=$(cat $DIRECTORIO_BRILLO/max_brightness)
+	# Nunca es necesario acceder a este archivo
+	
+	
 if [ $1 == "get" ]; then
 	if [ $2 == "current" ]; then
-		cat $BRILLO_ACTUAL
-	elif [ $2 == "max" ]; then
-		cat $BRILLO_MAXIMO
+		awk -v br=$BRILLO_ACTUAL -v brm=$BRILLO_MAXIMO 'BEGIN {print int((br/brm)*100)}'
 	fi
+	
 elif [ $1 == "set" ]; then
-	echo $2 > $BRILLO_ACTUAL
+	awk -v set=$2 -v brm=$BRILLO_MAXIMO 'BEGIN {print int(set*(brm/100))-1}' > $BRA_DIR
 elif [ $1 == "add" ]; then
 	awk -v val=$2 '{print $1 + val}' $BRILLO_ACTUAL > $BRILLO_ACTUAL
 fi
+
+
