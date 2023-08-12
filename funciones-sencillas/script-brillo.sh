358ec64dc86ef8be70c95724a09bc85c196c8446
digitar120 <digitar120@outlook.com>
2023-08-12 03:43:22 -0300

    Correcciones a script-brillo

--- funciones-sencillas/script-brillo.sh
+++ funciones-sencillas/script-brillo.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 . ../configuraciones/variables.sh
-BRA_DIR=$DIRECTORIO_BRILLO/brightness
-BRILLO_ACTUAL=$(cat $DIRECTORIO_BRILLO/brightness)
-BRILLO_MAXIMO=$(cat $DIRECTORIO_BRILLO/max_brightness)
-	# Nunca es necesario acceder a este archivo
+BR_DIR=$DIRECTORIO_BRILLO/brightness
+BR=$(cat $DIRECTORIO_BRILLO/brightness)
+BRM=$(cat $DIRECTORIO_BRILLO/max_brightness)
 	
 	
 if [ $1 == "get" ]; then
 	if [ $2 == "current" ]; then
-		awk -v br=$BRILLO_ACTUAL -v brm=$BRILLO_MAXIMO 'BEGIN {print int((br/brm)*100)}'
+		awk -v br=$BR -v brm=$BRM 'BEGIN {print int((br/brm)*100)}'
 	fi
 	
 elif [ $1 == "set" ]; then
-	awk -v set=$2 -v brm=$BRILLO_MAXIMO 'BEGIN {print int(set*(brm/100))-1}' > $BRA_DIR
+	awk -v set=$2 -v brm=$BRM 'BEGIN {print int(set*(brm/100))-1}' > $BR_DIR
 elif [ $1 == "add" ]; then
-	awk -v val=$2 '{print $1 + val}' $BRILLO_ACTUAL > $BRILLO_ACTUAL
+	echo $BR | awk -v val=$2 '{print $1 + val}' > $BR_DIR
 fi
 
 
