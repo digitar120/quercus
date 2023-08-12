a691eeb91be3c3c2e5372caf8d51de1ed53f2759
digitar120 <digitar120@outlook.com>
2023-08-12 03:45:46 -0300

    Correcciones a script-brillo

--- funciones-sencillas/script-brillo.sh
+++ funciones-sencillas/script-brillo.sh
@@ -7,12 +7,11 @@ BRM=$(cat $DIRECTORIO_BRILLO/max_brightness)
 	
 	
 if [ $1 == "get" ]; then
-	if [ $2 == "current" ]; then
-		awk -v br=$BR -v brm=$BRM 'BEGIN {print int((br/brm)*100)}'
-	fi
+	awk -v br=$BR -v brm=$BRM 'BEGIN {print int((br/brm)*100)}'
 	
 elif [ $1 == "set" ]; then
 	awk -v set=$2 -v brm=$BRM 'BEGIN {print int(set*(brm/100))-1}' > $BR_DIR
+	
 elif [ $1 == "add" ]; then
 	echo $BR | awk -v val=$2 '{print $1 + val}' > $BR_DIR
 fi
