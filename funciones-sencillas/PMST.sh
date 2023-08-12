d766596599c6505fdecde1eaeb9cfa32c2e86261
digitar120 <digitar120@outlook.com>
2023-08-12 04:06:58 -0300

    Cambios a PMST en pos de concentrar todas las funciones relacionadas a recursos en un mismo archivo

--- funciones-sencillas/PMST.sh
+++ funciones-sencillas/PMST.sh
@@ -1,3 +1,31 @@
-. ./procesador.sh
-. ./memoria.sh
-. ./temperatura.sh
+#. ./procesador.sh
+#. ./memoria.sh
+#. ./temperatura.sh
+
+ProcUse(){
+top -bn1 | awk '
+NR==3 {
+	print "PROC " $2	
+}'
+}
+
+ProcWarn(){
+top -bn1 | \
+	awk 'NR==3 {
+		if ($2 > 75)
+			print "P"
+		else
+			print "-"
+	}
+' | tr '\n' ' '
+}
+
+if [[ $1 == "proc" && $2 == "warn" ]]; then
+	ProcWarn;
+
+elif [[ $1 == "proc" && $2 == "use" ]]; then
+	ProcUse;
+else
+return 1;
+fi
+
