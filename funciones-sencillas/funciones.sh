c8fce39f9529cbf21c26527f8ba96551c86c4724
digitar120 <digitar120@outlook.com>
2023-08-12 22:31:08 -0300

    Cambio de nombre de PMST

---
+++ funciones-sencillas/funciones.sh
@@ -0,0 +1,171 @@
+. ../configuraciones/variables.sh
+
+BatStatus(){
+local CARGA_ACTUAL=$DIRECTORIO_BATERIA/charge_now
+local CARGA_COMPLETA=$DIRECTORIO_BATERIA/charge_full
+local ESTADO=$DIRECTORIO_BATERIA/status
+
+cat $CARGA_ACTUAL \
+	$CARGA_COMPLETA \
+	$ESTADO \
+	| tr '\n' ' ' | awk '
+		{
+			chargepc= int(($1/$2)*100)
+			if ($3=="Charging")
+				print "BAT " chargepc ", CARGANDO"
+
+			else
+				print "BAT " chargepc
+		}' 
+}
+
+ProcUse(){
+	top -bn1 | awk '
+	NR==3 {
+		print "PROC " $2	
+	}'
+	}
+
+	ProcWarn(){
+	top -bn1 | \
+		awk 'NR==3 {
+			if ($2 > 75)
+				print "P"
+			else
+				print "-"
+		}
+	' | tr '\n' ' '
+}
+
+BarMemWarn(){
+	free -m | awk '
+	NR==2 {
+		mempc=int(($3/$2)*100)
+		if (mempc >= 90)
+			print "M" mempc
+		else
+			print "-"
+	}
+	NR==3 {
+		swappc=int(($3/$2)*100)
+		if (swappc >= 10)
+			print "S" swappc
+		else
+			print "-"
+		
+	}' | tr '\n' ' '
+}
+
+MemWarn(){
+free -m | awk '
+	NR==2 {
+		mempc=int(($3/$2)*100)
+		print "MEM " mempc
+	}
+	NR==3 {
+		swappc=int(($3/$2)*100)
+		print "SWAP " swappc
+	}
+	'
+}
+
+FullDateTime(){
+	date "+%H:%M, %A %d de %B"
+}
+
+IPs(){
+	ip a | awk '/inet/ && $2 !~ /127.0.0.1/ {
+		print $NF " " substr($2,1,match($2,"/")-1)
+	}'
+}
+
+ClimaAcqOnce(){
+	CLIMA=$(w3m -dump -4 tn.com.ar/clima | awk 'NR==16 {print $1}');
+}
+
+ClimaAcq () {
+	while :
+		do
+		ClimaAcqOnce;
+		sleep 320;
+	done
+}
+
+ClimaPrint(){
+	echo $CLIMA
+}
+
+case $1 in
+	proc)
+	case $2 in
+		warn)
+			ProcWarn
+		;;
+		use)
+			ProcUse
+		;;
+		*)
+		exit 1
+		;;
+	esac
+	;;
+	mem)
+		case $2 in
+			#use)
+			#;;
+			warn)
+				MemWarn
+			;;
+			barwarn)
+				BarMemWarn
+			;;
+			*)
+			exit 1
+			;;
+		esac
+	;;
+	time)
+		case $2 in
+		full)
+			FullDateTime
+		;;
+		*)
+			exit 1
+		;;
+		esac
+	;;
+	bat)
+		BatStatus
+	;;
+	
+	ips)
+		IPs
+	;;
+	
+	clima)
+		case $2 in
+		
+		"exec")
+			ClimaAcq
+		;;
+		
+		"exec_once")
+			ClimaAcqOnce
+		;;
+		
+		get)
+			ClimaPrint
+		;;
+		
+		*)
+			exit 1
+		;;
+		esac
+	;;
+	
+	*)
+	exit 1
+	;;
+esac
+	
+
