a9df2d60b32a8789ea6c567970aef5a3fd1ddeb2
digitar120 <digitar120@outlook.com>
2023-08-20 00:44:44 -0300

    Imprimir mensajes de estado y hora, y pipear todo a un log

--- scripts/Obsidian/obsidian-sync-powershell.ps1
+++ scripts/Obsidian/obsidian-sync-powershell.ps1
@@ -4,7 +4,16 @@ $PROVIDER=""
 
 Switch ($args[0])
 {
-    exec 	{while ($true){rclone bisync ${PROVIDER}:${REMOTE_VAULT} $LOCAL_VAULT >> obsidian-sync-log.txt}}
+    exec 	{
+	while ($true){
+		Get-Date >> obsidian-sync-log.txt
+		echo "EJECUTANDO" >> obsidian-sync-log.txt
+		rclone bisync ${PROVIDER}:${REMOTE_VAULT} $LOCAL_VAULT >> obsidian-sync-log.txt
+		
+		Get-Date >> obsidian-sync-log.txt
+		echo "ESPERANDO" >> obsidian-sync-log.txt
+		Start-Sleep -Seconds 60 >> obsidian-sync-log.txt
+	}}
     resync 	{rclone bisync --resync ${PROVIDER}:${REMOTE_VAULT} $LOCAL_VAULT}
     default {exit 1}
 }
