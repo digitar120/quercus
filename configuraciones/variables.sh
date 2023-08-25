9b97f3e7d196c47b8fd5cda4f01582fdaedeb5ed
digitar120 <digitar120@outlook.com>
2023-08-25 15:32:46 -0300

    Nueva variable para expresar el directorio de scripts

--- configuraciones/variables.sh
+++ configuraciones/variables.sh
@@ -1,3 +1,4 @@
 DIRECTORIO_BRILLO=/sys/class/backlight/radeon_bl0
-DIRECTORIO_BATERIA=/sys/class/power_supply/BAT0
-DIR_FUNC=.
+DIRECTORIO_BATERIA=/sys/class/power_supply/BAT0	
+RP_SCRIPT_DIR=$HOME/Scripts/funciones-linux/configuraciones
+
