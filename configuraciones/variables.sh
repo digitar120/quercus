e26c93f79e8c61de565c166bdd94c39355f821c9
digitar120 <digitar120@outlook.com>
2023-08-26 13:50:58 -0300

    Simplificar acceso al script de funciones

--- configuraciones/variables.sh
+++ configuraciones/variables.sh
@@ -1,4 +1,3 @@
 DIRECTORIO_BRILLO=/sys/class/backlight/radeon_bl0
 DIRECTORIO_BATERIA=/sys/class/power_supply/BAT0	
-RP_SCRIPT_DIR=$HOME/Scripts/funciones-linux/configuraciones
-
+FUNCIONES=$HOME/Scripts/funciones-linux/configuraciones/funciones.sh
