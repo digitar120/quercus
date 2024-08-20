107b6990859028396a5f6a74cc949c1b27939695
digitar120 <digitar120@outlook.com>
2024-08-19 21:32:52 -0300

    Borrar elementos viejos

--- scripts/package-managers/package-list-backup-fish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/fish
-set HORA (date +%Y-%m-%d-%H-%M-%S)
-set HOSTNAME (uname -n)
-
-pacman -Qet > pacman-instalados-explícitamente-$HOSTNAME-$HORA.txt
--- scripts/package-managers/package-list-backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-HORA=$(date +%Y-%m-%d-%H-%M-%S)
-HOSTNAME=$(uname -n)
-
-pacman -Qet > pacman-instalados-explícitamente-$HOSTNAME-$HORA.txt
